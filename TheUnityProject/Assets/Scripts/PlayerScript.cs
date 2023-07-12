bd75887b8a9644ae1d0a65adecd7df3e6b6b0030
Maxime Birkkjær Havez <maxih@hotmail.dk>
2023-07-12 11:42:09 +0200

    Finished jumping mechanic

--- TheUnityProject/Assets/Scripts/PlayerScript.cs
+++ TheUnityProject/Assets/Scripts/PlayerScript.cs
@@ -9,12 +9,16 @@ public class PlayerScript : MonoBehaviour
     [SerializeField]
     float turnSpeed = 1f;
     [SerializeField]
+    float jumpForce = 1f;
+    [SerializeField]
     Rigidbody body;
 
     Transform trans;
 
     float moveInput;
     float turnInput;
+    bool jumpInput;
+    bool onGround;
     
     // Start is called before the first frame update
     void Start()
@@ -27,15 +31,30 @@ public class PlayerScript : MonoBehaviour
     {
         moveInput = Input.GetAxis("Vertical");
         turnInput = Input.GetAxis("Horizontal");
+        jumpInput = Input.GetButtonDown("Jump");
 
+        if (jumpInput == true && onGround == true)
+        {
+            onGround = false;
+            body.velocity = Vector3.up * jumpForce;
+        }
     }
 
     private void FixedUpdate()
     {
         trans.Rotate(Vector3.up, turnSpeed * turnInput);
 
+
         Vector3 moveVector = trans.forward * speed * moveInput;
         moveVector.y = body.velocity.y;
         body.velocity = moveVector;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Jord"))
+        {
+            onGround = true;
+        }
     }
 }
